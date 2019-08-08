42ee87eea65641b0ef9b24c6e2c5e561206e3d96
Fashola Ayodeji <fashtop3@gmail.com>
2019-08-08 13:06:33 +0100

    inital

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          New project status
         </a>
       </header>
     </div>
